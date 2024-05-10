2abcac11385732527bce092845b870fa597e9532
Dailson Gomes <dailson@localhost.localdomain>
2024-05-10 15:16:22 -0300

    Add prisma filter exception

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HandlerExceptionFilter } from './handler-exception/handler-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new HandlerExceptionFilter(httpAdapter));
+
+  // Validation and Transformation
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Convert the requet to the approriate object
