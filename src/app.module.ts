9f85962f4e53556aebf03d737fa09aa9082a2ba7
Dailson Gomes <dailson@localhost.localdomain>
2024-05-13 17:57:33 -0300

    Organize the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { classes } from '@automapper/classes';
 import { AutomapperModule } from '@automapper/nestjs';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { ArticleModule } from './article/article.module';
-import { AuthModule } from './auth/auth.module';
-import { JwtAuthGuard } from './auth/guard/jwt.guard';
-import { RolesGuard } from './auth/guard/role.guard';
-import { FileModule } from './file/file.module';
-import { UserModule } from './user/user.module';
+import { ArticleModule } from './module/article/article.module';
+import { AuthModule } from './module/auth/auth.module';
+import { JwtAuthGuard } from './module/auth/guard/jwt.guard';
+import { RolesGuard } from './module/auth/guard/role.guard';
+import { FileModule } from './module/file/file.module';
+import { UserModule } from './module/user/user.module';
 @Module({
   imports: [
     // AUTOMAPPER CONFIGURATION
--- src/module/file/file.controller.ts
+++ src/module/file/file.controller.ts
@@ -3,7 +3,7 @@ import { InjectMapper } from '@automapper/nestjs';
 import { Controller, Post, Req, UploadedFile } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
-import { FileUpload } from './decorator/file-upload.decorator';
+import { FileUpload } from '../../decorator/file-upload.decorator';
 import { FileReadDTO } from './dto/file-read.dto';
 import { FileEntity } from './entity/file.entity';
 import { FileService } from './file.service';
